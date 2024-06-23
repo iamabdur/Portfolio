4c68a45622e19692b0a309d2b78a410e15df6eba
iamabdur <abdurrahmank2002@gmail.com>
2024-06-23 17:39:13 +0530

    push all

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,20 +38,22 @@ $(document).ready(function () {
     });
 
     // <!-- emailjs to mail contact form data -->
-    $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+$("#contact-form").submit(function(event) {
+                event.preventDefault(); // Prevent the default form submission
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
+                emailjs.init("xZDoM_sHJpleLdtk8"); // Initialize EmailJS with your user ID
+
+                emailjs.sendForm('service_ef8mgk1', 'template_x8ipgcb', '#contact-form')
+                    .then(function(response) {
+                        
+                        console.log('SUCCESS!', response.status, response.text);
+                        document.getElementById("contact-form").reset(); // Reset the form
+                        alert("Form Submitted Successfully");
+                    }, function(error) {
+                        console.error('FAILED...', error);
+                        alert("Form Submission Failed! Try Again");
+                    });
             });
-        event.preventDefault();
-    });
     // <!-- emailjs to mail contact form data -->
 
 });
@@ -59,7 +61,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Abdur Rahman";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -191,15 +193,7 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
+
 // End of Tawk.to Live Chat
 
 
